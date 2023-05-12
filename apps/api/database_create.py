230739997c7d33d7c7ff9a10a53068b0a6795af0
RafaelMFerreira <59070941+RafaelMFerreira@users.noreply.github.com>
2023-05-12 20:17:25 -0300

    gets basicos

---
+++ apps/api/database_create.py
@@ -0,0 +1,65 @@
+from sqlalchemy import create_engine, Table, Column, Integer, Float, String, Date, MetaData, ForeignKey
+engine = create_engine('mssql+pyodbc://sa:123@localhost/bagitup?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes', echo=True)
+meta = MetaData()
+
+usuario = Table(
+   'usuario', meta, 
+   Column('id_usuario', Integer, primary_key = True), 
+   Column('InSexo', String), 
+   Column('DtNascimento', Date), 
+   Column('NmUsuario', String), 
+   Column('InFilhos', String)
+)
+
+item = Table(
+   'item', meta, 
+   Column('id_item', Integer, primary_key=True),
+   Column('InCategoria', String), 
+   Column('VlVolumeItem', Float), 
+   Column('VlPeso', Float), 
+   Column('NmItem', String)
+)
+
+mala = Table(
+   'mala', meta, 
+   Column('id_mala', Integer, primary_key = True), 
+   Column('id_item', Integer, ForeignKey(item.c.id_item)), 
+   Column('id_usuario', Integer, ForeignKey(usuario.c.id_usuario))
+)
+
+viagem = Table(
+   'viagem', meta, 
+   Column('id_viagem', Integer, primary_key = True), 
+   Column('id_usuario', Integer, ForeignKey(usuario.c.id_usuario)), 
+   Column('NmLocalDestino', String), 
+   Column('DtInicioViagem', Date),
+   Column('DtFimViagem', Date),
+   Column('InMeioTransporte', String),
+   Column('InTipoViagem', String)
+)
+
+viagem_mala = Table(
+   'viagem_mala', meta, 
+   Column('id_mala', Integer, ForeignKey(mala.c.id_mala), primary_key = True), 
+   Column('id_viagem', Integer, ForeignKey(viagem.c.id_viagem), primary_key = True)
+)
+
+clima_dia = Table(
+   'clima_dia', meta, 
+   Column('id_viagem', Integer, ForeignKey(viagem.c.id_viagem), primary_key = True), 
+   Column('DtViagem', Date, primary_key = True), 
+   Column('VlTemperatura', Integer), 
+   Column('VlSensacaoTermica', Integer),
+   Column('NmClimaPredominante', String)
+)
+
+meta.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# export the variables representing the tables
+usuario_table = usuario
+item_table = item
+mala_table = mala
+viagem_table = viagem
