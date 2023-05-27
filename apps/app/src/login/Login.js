64539af77e6755e97b6c43181d04675945457fee
Igor Soler <49961203+IgorSolerC@users.noreply.github.com>
2023-05-26 21:30:54 -0300

    Add files via upload

--- apps/app/src/login/Login.js
+++ apps/app/src/login/Login.js
@@ -8,6 +8,25 @@ function Login({onClickOutside}) {
 
     const navigate = useNavigate();
 
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      
+    }, []);
+
+
+
+    function connect(){
+        fetch('http://localhost:5000/login/'+data['user']+'/'+data['pwd'])
+        .then(response => response.text())
+        .then(text => {
+            console.log(text)
+            if(text == '1'){
+                navigate('/create')
+            }
+        });
+    }
+
     return (
         <>
             <div id='backgound-login' className='blurry-background' onClick={onClickOutside}>
@@ -16,15 +35,19 @@ function Login({onClickOutside}) {
                 <div id='login-title'>Login</div>
                 <div className='login-input-div'>
                     <span className='login-input-title'>Nickname</span>
-                    <input className='login-input'></input>
+                    <input className='login-input'
+                        onChange={(e) => setData({...data, 'user':e.target.value})}
+                    ></input>
                 </div>
                 <div className='login-input-div'>
                     <span className='login-input-title'>Password</span>
-                    <input className='login-input' type='password'></input>
+                    <input className='login-input' type='password'
+                        onChange={(e) => setData({...data, 'pwd':e.target.value})}
+                    ></input>
                 </div>
                 <div className='login-submit-div'>
-                    <button className='login-submit' onClick={() => navigate('/create')}>Entrar</button>
-                    <button className='login-submit' onClick={() => navigate('/create')}>Cadastrar</button>
+                    <button className='login-submit' onClick={() => connect()}>Entrar</button>
+                    <button className='login-submit' onClick={() => ''}>Cadastrar</button>
                 </div>
             </div>
         </>
