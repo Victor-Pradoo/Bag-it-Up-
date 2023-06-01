dac7fb8d71bb57ae02d6833017ad1f9ebcfed38f
IgorSolerC <igorsoler.c@gmail.com>
2023-06-01 03:40:58 -0300

    Viagens criadas são salvas no banco!

--- apps/api_flask/app.py
+++ apps/api_flask/app.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import pandas as pd
 
@@ -8,7 +8,6 @@ userdb = "teste"
 passworddb = "teste"
 ip = "3.208.119.152:5432"
 url_connection = f'postgresql://{userdb}:{passworddb}@{ip}/bag'
-
 engine = create_engine(url_connection, echo=True)
 
 DEVELOPMENT_ENV = True
@@ -71,6 +70,46 @@ def register(user, pwd):
     print(id_usuario)
 
     return str(id_usuario)
+
+@app.route("/nova_viagem", methods=['POST'])
+def nova_viagem():
+    def add_left_zero(x):
+        x = str(x)
+        if len(x) == 1:
+            x = f'0{x}'
+        return x
+
+    data = request.json
+
+    new_row = {
+        "id_usuario": data['user_id'],
+        "dt_partida": f"'{data['partida']['ano']}-{add_left_zero(data['partida']['mes'])}-{add_left_zero(data['partida']['dia'])}'", 
+        "dt_volta":  f"'{data['volta']['ano']}-{add_left_zero(data['volta']['mes'])}-{add_left_zero(data['volta']['dia'])}'",
+        "nm_temperatura": f"'{data['temperatura']}'", 
+        "nm_local_destino": f"'{data['local']}'", 
+        'has_festa': str('festa' in data['vestimentas']).lower(),
+        'has_trabalho': str('trabalho' in data['vestimentas']).lower(),
+        'has_piscina': str('piscina' in data['vestimentas']).lower(),
+        'has_esporte': str('esporte' in data['vestimentas']).lower(),
+        'has_maquina_lavar': str('maquina_lavar' in data['outros']).lower(),
+    }
+
+    ### Cadastra novo user
+    keys = new_row.keys()
+    query = f"""
+        INSERT INTO viagem ({' , '.join(keys)})
+        VALUES ({' , '.join([str(v) for k, v in new_row.items()])});
+    """
+    conn = engine.connect()
+    conn.execute(text(
+        query
+    ))
+    conn.commit()
+    conn.close()
+
+    print(query)
+
+    return '1'
 
 if __name__ == "__main__":
     app.run(debug=DEVELOPMENT_ENV)
--- apps/app/src/mala/Create.js
+++ apps/app/src/mala/Create.js
@@ -1,16 +1,40 @@
 import React, { Children, useRef } from "react";
 import {useState, useEffect} from 'react';
 import { useNavigate } from "react-router-dom";
+import Globals from "../Globals"
 
-
-function DateSelect(){
+function DateSelect({onValidInput}){
     const dias = Array.from(Array(31), (_, index) => index + 1);
     const meses = Array.from(Array(12), (_, index) => index + 1);
     const anos = Array.from(Array(20), (_, index) => index + 2023); // Ta harcoded o ano inicial
 
+    const [data, setData] = useState(
+        {
+            dia:null,
+            mes:null,
+            ano:null,
+        }
+    )
+
+    function onChange(e, key){
+        var val = e.target.value
+        data[key] = val
+        setData({...data})
+
+        if(data.dia != null
+           && data.mes != null
+           && data.ano != null){
+            onValidInput(data)
+        }
+    }
+
     return(
         <>
-        <select className="mala-input mala-date-input" style={{marginLeft:'0'}}> {/* Sim, é possível selecionar dia 31 de fevereiro... */}
+        <select
+            className="mala-input mala-date-input"
+            style={{marginLeft:'0'}}
+            onChange={(e) => onChange(e, 'dia')}
+        > {/* Sim, é possível selecionar dia 31 de fevereiro... */}
             <option disabled selected value={-1}>Dia</option>
             {dias.map((dia, idx) => {
                 return(
@@ -18,7 +42,10 @@ function DateSelect(){
                     )
                 })}
         </select>
-        <select className="mala-input mala-date-input"> {/* Sim, é possível selecionar dia 31 de fevereiro... */}
+        <select
+            className="mala-input mala-date-input"
+            onChange={(e) => onChange(e, 'mes')}
+        > {/* Sim, é possível selecionar dia 31 de fevereiro... */}
             <option disabled selected value={-1}>Mês</option>
             {meses.map((mes, idx) => {
                 return(
@@ -26,7 +53,10 @@ function DateSelect(){
                     )
                 })}
         </select>
-        <select className="mala-input mala-date-input"> {/* Sim, é possível selecionar dia 31 de fevereiro... */}
+        <select
+            className="mala-input mala-date-input"
+            onChange={(e) => onChange(e, 'ano')}
+        > {/* Sim, é possível selecionar dia 31 de fevereiro... */}
             <option disabled selected value={-1}>Ano</option>
             {anos.map((ano, idx) => {
                 return(
@@ -37,7 +67,7 @@ function DateSelect(){
         </>
     )
 }
-function Checkbox({text}){
+function Checkbox({text, onChange}){
     return(
         <div style={{display: 'block'}}>
         <label className="mala-label">{text}</label>
@@ -45,6 +75,7 @@ function Checkbox({text}){
                 type={'checkbox'}
                 className="mala-checkbox"
                 style={{float:'right'}}   
+                onChange={onChange}
                 >                    
             </input>
         </div>
@@ -54,6 +85,46 @@ function Checkbox({text}){
 function Create() {
 
     const navigate = useNavigate()
+    const [inputedData, setInputedData] = useState(
+        {
+            user_id: Globals.USER_ID,
+            partida: null,
+            volta: null,
+            temperatura: null,
+            local: null,
+            vestimentas: [],
+            outros: [],
+        }
+    )
+
+    function gerarMala(){
+        console.log(inputedData)
+
+        if (inputedData.partida === null
+            || inputedData.volta === null
+            || inputedData.temperatura === null
+            || inputedData.local === null
+        ){
+            console.log('faltam itens!')
+        } else {
+            
+            fetch('http://localhost:5000/nova_viagem', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+            },
+                body: JSON.stringify(inputedData)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                navigate('/historico/Nova%20Viagem')
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
+    }
 
     return (
         <>
@@ -62,44 +133,128 @@ function Create() {
                 <div className="mala-question-title">Sobre a sua viagem</div>
                 <div className="mala-question">
                     <div>Partida</div>
-                    <DateSelect/>
+                    <DateSelect
+                        onValidInput={(obj) => {
+                            inputedData.partida = obj
+                            setInputedData({...inputedData})
+                        }}
+                    />
                     <div style={{marginTop: '8px'}}>Volta</div>
-                    <DateSelect/>
+                    <DateSelect
+                        onValidInput={(obj) => {
+                            inputedData.volta = obj
+                            setInputedData({...inputedData})
+                        }}
+                    />
                 </div>
                 <div className="mala-question">
                     <div>Temperatura</div>
-                    <select className="mala-input">
+                    <select
+                        className="mala-input"
+                        onChange={(e) => {
+                            let val = e.target.value
+                            inputedData.temperatura = val
+                            setInputedData({...inputedData})
+                        }}
+                    >
                         <option disabled selected value={-1}>Selecione</option>
-                        <option value={0}>{'Muito Frio 5°C'}</option>
-                        <option value={1}>{'Frio 15°C'}</option>
-                        <option value={2}>{'Fresco 20°C'}</option>
-                        <option value={3}>{'Equilibrado 25°C'}</option>
-                        <option value={4}>{'Quente 30°C'}</option>
-                        <option value={5}>{'Muito Quente 35°C'}</option>
+                        <option value={'muito frio'}>{'Muito Frio 5°C'}</option>
+                        <option value={'frio'}>{'Frio 15°C'}</option>
+                        <option value={'fresco'}>{'Fresco 20°C'}</option>
+                        <option value={'equilibrado'}>{'Equilibrado 25°C'}</option>
+                        <option value={'quente'}>{'Quente 30°C'}</option>
+                        <option value={'muito quente'}>{'Muito Quente 35°C'}</option>
                     </select>
                 </div>
                 <div className="mala-question">
                     <div>Local</div>
-                    <select className="mala-input">
+                    <select
+                        className="mala-input"
+                        onChange={(e) => {
+                            let val = e.target.value
+                            inputedData.local = val
+                            setInputedData({...inputedData})
+                        }}
+                    >
                         <option disabled selected value={-1}>Selecione</option>
-                        <option value={0}>{'Cidade'}</option>
-                        <option value={1}>{'Navio'}</option>
-                        <option value={2}>{'Natureza'}</option>
-                        <option value={3}>{'Praia'}</option>
+                        <option value={'cidade'}>{'Cidade'}</option>
+                        <option value={'navio'}>{'Navio'}</option>
+                        <option value={'natureza'}>{'Natureza'}</option>
+                        <option value={'praia'}>{'Praia'}</option>
                     </select>
                 </div>
                 <div className="mala-question">
                     <div>Vestimentas</div>
-                    <Checkbox text='Festa'/>
-                    <Checkbox text='Trabalho'/>
-                    <Checkbox text='Piscina'/>
-                    <Checkbox text='Esporte'/>
+                    <Checkbox text='Festa'
+                        onChange={(e) => {
+                            if(e.target.checked){
+                                inputedData.vestimentas.push('festa')
+                            } else {
+                                if(inputedData.vestimentas.includes('festa')){
+                                    const index = inputedData.vestimentas.indexOf('festa');
+                                    inputedData.vestimentas.splice(index, 1);
+                                }
+                            }
+                            setInputedData({...inputedData})
+                        }}
+                    />
+                    <Checkbox text='Trabalho'
+                        onChange={(e) => {
+                            if(e.target.checked){
+                                inputedData.vestimentas.push('trabalho')
+                            } else {
+                                if(inputedData.vestimentas.includes('trabalho')){
+                                    const index = inputedData.vestimentas.indexOf('trabalho');
+                                    inputedData.vestimentas.splice(index, 1);
+                                }
+                            }
+                            setInputedData({...inputedData})
+                        }}
+                    />
+                    <Checkbox text='Piscina'
+                        onChange={(e) => {
+                            if(e.target.checked){
+                                inputedData.vestimentas.push('piscina')
+                            } else {
+                                if(inputedData.vestimentas.includes('piscina')){
+                                    const index = inputedData.vestimentas.indexOf('piscina');
+                                    inputedData.vestimentas.splice(index, 1);
+                                }
+                            }
+                            setInputedData({...inputedData})
+                        }}
+                    />
+                    <Checkbox text='Esporte'
+                        onChange={(e) => {
+                            if(e.target.checked){
+                                inputedData.vestimentas.push('esporte')
+                            } else {
+                                if(inputedData.vestimentas.includes('esporte')){
+                                    const index = inputedData.vestimentas.indexOf('esporte');
+                                    inputedData.vestimentas.splice(index, 1);
+                                }
+                            }
+                            setInputedData({...inputedData})
+                        }}
+                    />
                 </div>
                 <div className="mala-question">
                     <div>Outros</div>
-                    <Checkbox text='Maquina de lavar roupas'/>
+                    <Checkbox text='Maquina de lavar roupas'
+                        onChange={(e) => {
+                            if(e.target.checked){
+                                inputedData.outros.push('maquina_lavar')
+                            } else {
+                                if(inputedData.outros.includes('maquina_lavar')){
+                                    const index = inputedData.outros.indexOf('maquina_lavar');
+                                    inputedData.outros.splice(index, 1);
+                                }
+                            }
+                            setInputedData({...inputedData})
+                        }}
+                    />
                 </div>
-                <div className="mala-finish-button" onClick={() => navigate('/historico/Nova%20Viagem')}>Gerar mala</div>
+                <div className="mala-finish-button" onClick={() => gerarMala()}>Gerar mala</div>
             </div>
         </>
          
